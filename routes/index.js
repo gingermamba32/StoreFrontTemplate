b98f1c43d5615f8503be9cdc6b1be2f97c3edbba
Michael_Montero <mmontero2012@gmail.com>
2016-01-26 18:13:19 -0700

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+process.env.PWD = process.cwd()
 
 /* GET home page. */
 var html_dir = '../views/';
 router.get('/', function(req, res, next) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(process.env.PWD+'/index.html'));
   //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
 });
 
